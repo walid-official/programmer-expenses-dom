2cd8611f1b3601212670e6bcde9c3bfa10cfe9ac
walid hasan <walid.official8@gmail.com>
2024-09-22 16:42:26 +0600

    some features are added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const remaining = document.getElementById('remaining-balance');
 const resultContainer = document.getElementById('results');
 const assistantTab = document.getElementById('assistant-tab');
 const historyTab = document.getElementById('history-tab');
+const expenseForm = document.getElementById('expense-form');
+const historySection = document.getElementById('history-section');
+const historyList = document.getElementById('history-list');
+
 
 calculate_btn.addEventListener('click', function(){
 
@@ -34,6 +38,15 @@ calculate_btn.addEventListener('click', function(){
     incomeBalance.innerText = balance_after_income.toFixed(2);
     // incomeInnerValue = softwareValue + coursesValue + internetValue;
 
+    let history = `
+            <div class="">
+              <h2>${new Date().toLocaleDateString()}</h2>
+              <h2>Income ${incomeValue}</h2>
+              <h2>Expenses ${incomeExpenses.innerText}</h2>
+              <h2>Balance ${incomeBalance.innerText}</h2>
+            </div>
+    `
+    historyList.innerHTML += history;
 });
 
 savings_btn.addEventListener('click', function(){
@@ -55,9 +68,23 @@ savings_btn.addEventListener('click', function(){
 assistantTab.addEventListener('click', function(event){
     assistantTab.classList.add('bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
     historyTab.classList.remove('bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+    expenseForm.classList.remove('hidden');
 })
+
+
 
 historyTab.addEventListener('click', function(event){
     historyTab.classList.add('bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
     assistantTab.classList.remove('bg-gradient-to-r', 'from-blue-500', 'to-purple-600', 'text-white');
+    expenseForm.classList.add('hidden');
+    historySection.classList.remove('hidden');
 })
+
+
+
+
+// Error handling
+let incomeValue = inputValueById('income');
+if(incomeValue < 0){
+    console.log('hello world')
+}
